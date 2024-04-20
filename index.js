1509dc8d62d2db4d8bd269d796a3dc8e6f2d76f3
Haniel Ryan <143314778+hanielRyan@users.noreply.github.com>
2024-04-20 13:34:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors({
-    origin: 'https://aaghum-ministries.vercel.app'
+    origin: 'https://aaghumministries.vercel.app'
 }));
 app.use(express.json());
 
