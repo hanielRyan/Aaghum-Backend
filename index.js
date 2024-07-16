d5386fba97cc390ef3b3beb6b1c0925b37b31562
Haniel Ryan <hanielryanephin@gmail.com>
2024-07-16 20:42:29 +0400

    v2

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ app.use(express.json());
 
 app.use("/",require("./mail"));
 
-app.get("/message",(req,res)=>{
+app.get("/",(req,res)=>{
+    res.set("Access-Control-Allow-Origin","https://aaghumministries.vercel.app");
     res.send("hi")
 })
 
-app.use((err,req,res,next)=>{
-    console.log(err);
-    res.status(err.status).json(err.message);
-})
 
 app.listen(5000,()=>console.log('server is running'));
--- mail.js
+++ mail.js
@@ -6,42 +6,24 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user:"hanielryanephin@gmail.com",
-        pass:process.env.GOOGLE_PASS
+        pass:process.env.MAIL_PASS
     }
 });
 
-router.post("/",(req,res,next)=>{
 
-    res.setHeader("Access-Control-Allow-Origin", "https://aaghumministries.vercel.app");
-
-    try{
-        const user  = req.body.data;
-        console.log(user)
-        const mailOptions = {
-            from:user.email,
-            to: 'aaghum.eh@gmail.com',
-            subject: user.subject,
-            html: `<h1>Name: ${user.firstName} ${user.lastName}</h1><h1>Email: ${user.email}</h1><h3>Message: ${user.message}</h3>`
-        };
-    
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                res.json(error)
-               const error =   new Error(error.message);
-               error.status = 404;
-               throw error;
-       
-            } else {
-                console.log('Email sent:'+ info.response);
-                 res.sendStatus(200);
-            }
-        });
-
-    }catch(err){
-  next(err);
-    }
+router.post("/",(req,res)=>{
+    const user = req.body.data;
+    transporter.sendMail({
+        from:user.email,
+        to:"hanielryanephin@gmail.com",
+        subject:user.subject,
+        html:`<div style="display:grid;gap:10px;place-items:center;"><h1>Email from ${user.firstName} ${user.lastName} with email ${user.email}.</h1>
+        <p>${user.message}</p>
+         </div>`
+    },(err,info)=>{
+        err ? console.log(err) : res.sendStatus(200);
+    });
 
 })
-
 
 module.exports = router;
